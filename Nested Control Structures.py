c57c19add580da5f2d41edd718f77e850b459d41
Xander <02Xander.Warner@gulllakecs.org>
2019-10-21 14:11:15 -0400

    Add Double For Loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -7,3 +7,9 @@ This program will nest a For Loop inside of another
 For Loop
 '''
 
+for i in range(3):
+    print("Outer For Loop: " + str(i))
+    for l in range(2):
+        print("     Inner For Loop: " + str(l))
+        
+
